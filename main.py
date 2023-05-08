8357a383fdae7f90912af69ee5c86e70af5088a5
Payal Moghariya <payal.moghariya@gmail.com>
2023-05-08 14:13:53 -0400

    Rock paper sciossors game

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
-player_choice = "rock"
-computer_choice = "paper"
+import random
+
+
+def get_choices():
+    player_choice = input("Enter a choice : Rock/Paper/Scissors : ")
+    
+    random_choice = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(random_choice)
+
+    choices = { "player":player_choice, "computer": computer_choice }
+    return choices
+
+def check_win(player, computer):
+    print(f"You chose : {player}  , Computer choice : {computer} ")
+    
+    if(player == computer):
+        return "Its a tie!"
+    
+    elif player == "Rock":
+        if computer == "Scissors":
+            return "You Win"
+        else:
+            return "You lose"
+        
+    elif player == "Paper":
+        if computer == "Rock":
+            return "You Win"
+        else:
+            return "You lose"
+    elif player == "Scissors":
+        if computer == "Paper":
+            return "You Win"
+        else:
+            return "You lose"
+        
+
+choices = get_choices()
+
+resolved = check_win(choices["player"],choices["computer"])
+print(resolved)
+
+
+
+        
+    
+
+
+    
+
+
