d4e2490f322054c89d069779b056e63f5cb2b39d
Payal Moghariya <payal.moghariya@gmail.com>
2023-05-10 09:57:57 -0400

    commit

---
+++ Learning.py
@@ -0,0 +1,12 @@
+#this code will have all the testing work
+try:
+    result = 2 / 0
+
+except ZeroDivisionError:
+    print("Number cant be divided by zero")
+    
+
+
+
+print(result)
+
---
+++ dog.py
@@ -0,0 +1,5 @@
+def bark():
+    print("barking")
+
+    
+
